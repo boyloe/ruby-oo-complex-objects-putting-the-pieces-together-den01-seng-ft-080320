9b81e0c418ce8757dad7c5deea850b5715011fc7
boyloe <boyloe@gmail.com>
2020-08-07 16:15:12 -0600

    Add Shoe class

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+    attr_accessor :author, :title, :page_count, :genre
+    page_count = 0
+    def initialize title
+        @title = title       
+        
+    end
 
+   def turn_page 
+    puts "Flipping the page...wow, you read fast!"
+   end
+
+    
+end
 
